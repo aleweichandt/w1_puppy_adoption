c7b4b353cc1c884366f7246ea0c18d7c7531d51b
alew <ale.w.es@sync.money>
2021-03-02 17:26:35 +0100

    Apply spotless

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/details/PuppyDetailsScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/details/PuppyDetailsScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.screen.details
 
 import androidx.compose.foundation.layout.Column
@@ -42,9 +57,12 @@ fun PuppyDetailsScreen(uuid: String?, back: () -> Unit) {
             },
         ) {
             puppy?.run {
-                PuppyDetailsScreenBody(puppy = puppy, onAdopt = {
-                    back()
-                })
+                PuppyDetailsScreenBody(
+                    puppy = puppy,
+                    onAdopt = {
+                        back()
+                    }
+                )
             } ?: run {
                 Text(text = stringResource(id = R.string.not_found))
             }
